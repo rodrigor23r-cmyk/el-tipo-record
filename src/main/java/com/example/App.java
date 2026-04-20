6c07a31c084c9dce3b03e3b1fd7cd21a79020b41
rodrigor23r-cmyk <rodrigor23r@gmail.com>
2026-04-20 13:42:08 +0200

    clase Persona creada como un record manualmente

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,17 @@
 package com.example;
 
-/**
- * Hello world!
- */
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+   
+       /* Persona instanciapersona = new Persona(); esto esta mal porque Persona es abstracta, no se puede instanciar
+         Una clase final no puede ser abstracta, por lo tanto no se puede instanciar, pero si se pueden crear objetos 
+         de esa clase, es decir, se pueden crear objetos de una clase final, pero no se pueden crear objetos de una clase abstracta.
+         Además, una clase final no puede ser heredada, por lo tanto no se pueden crear subclases de una clase final, pero si se pueden
+         crear subclases de una clase abstracta.
+         */
+
+        
+
+     }
 }
